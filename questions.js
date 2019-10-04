12a2072a2651484b73a9ea0f447e62f6a070e5ba
Lindsey Brown <libersword@gmail.com>
2019-10-04 13:52:43 -0600

    Basic structure for HTML, beginning the JS logic

---
+++ questions.js
@@ -0,0 +1,35 @@
+var questions = [
+  {
+    title: "Types of for loops DO NOT include:",
+    choices: ["for", "foreach", "for of", "for the love of javascript"],
+    answer: "for the love of javascript"
+  },
+  {
+    title: "What HTML element is javascript put into?",
+    choices: ["<js>", "<java>", "<script>", "<javascripts>"],
+    answer: "parentheses"
+  },
+  {
+    title: "How do you change HTML in javascript?",
+    choices: [".innerHTML", ".changeThis", ".textContent", ".newText"],
+    answer: "parentheses"
+  },
+  {
+    title: "The javascript should be loaded in the _____?",
+    choices: ["body tag", "what javascript", "head tag", "title tag"],
+    answer: "parentheses"
+  },
+  {
+    title: "Script file is called with _____?",
+    choices: ["href", "link", "javascript", "src"],
+    answer: "parentheses"
+  },
+  {
+    title: "Which of these creates a function?",
+    choices: ["function MyFunction()", "function.Myfunction()", "function = MyFunction()", "function {Function()}"],
+    answer: "parentheses"
+  }
+];
+
+//store these questions in local storage for retrieval 
+localStorage.setItem("questions", JSON.stringify(questions));
---
+++ script.js
@@ -0,0 +1,29 @@
+//timer stuff
+//quiz stuff
+
+//Declare all of the variables that exist in the HTML document to be populated with javascript
+
+//Declare variables to be used in the functions
+var score = 0;
+var questionIndex = 0;
+var questionArray = questions;
+
+
+//Quiz Functions
+//Start the quiz
+
+//Render the Questions and their choices
+
+//Listen for a button click, check that answer against the correct answer
+
+//If correct add 1 to the score and render the next question
+
+//If incorrect, subtract from timer and take away from the score then render the next question
+
+//do this for all questions until the entire array has been iterated through
+
+//render the highscore screen
+
+//Allow user input and store the initials with the score
+
+//allow for the quiz to start over
